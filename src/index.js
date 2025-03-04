55da1afbe8f281435da3937a81c667d097fa0933
Maria Minque Lopez Guillen <mariialg21@gmail.com>
2025-03-04 23:51:40 +0100

    Parte del index

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('index', { title: 'Joyas Preciosas' }));
 app.get('/inicio', (req, res) => res.render('index', { title: 'Joyas Preciosas' }));
-app.get('/contact', (req, res) => res.render('index', { title: 'Contacto-Joyas Preciosas' }));
+app.get('/contacto', (req, res) => res.render('contact', { title: 'Contacto-Joyas Preciosas' }));
+app.get('/populares', (req, res) => res.render('populares', { title: 'Populares-Joyas Preciosas' }));
+app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
