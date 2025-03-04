f3d2a9c6cbd2867a0ac06e495a28719919ff0041
Maria Minque Lopez Guillen <mariialg21@gmail.com>
2025-03-04 23:59:14 +0100

    Manejo de errores añadido

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ app.get('/', (req, res) => res.render('index', { title: 'Joyas Preciosas' }));
 app.get('/inicio', (req, res) => res.render('index', { title: 'Joyas Preciosas' }));
 app.get('/contacto', (req, res) => res.render('contact', { title: 'Contacto-Joyas Preciosas' }));
 app.get('/populares', (req, res) => res.render('populares', { title: 'Populares-Joyas Preciosas' }));
+// Manejo de errores 404
+app.use((req, res) => {
+    res.status(404).render('404', {
+        titulo: "Error 404",
+        descripcion: "Página no encontrada"
+    });
+});
 app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
