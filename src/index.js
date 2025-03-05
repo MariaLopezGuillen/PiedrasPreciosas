d6eecd7022bd8029543fc33606d7de3a3a374ef5
Maria Minque Lopez Guillen <mariialg21@gmail.com>
2025-03-05 19:19:22 +0100

    configuracion del mongoDB

--- src/index.js
+++ src/index.js
@@ -25,3 +25,31 @@ app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
 
+// Configuración de MongoDB
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://maria:Piedras123@piedras.sei4o.mongodb.net/?retryWrites=true&w=majority&appName=Piedras";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
